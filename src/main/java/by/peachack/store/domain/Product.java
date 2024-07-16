785d937579db22aafdc1fde34962ec64ade90494
PeaChack <dev.peachack@gmail.com>
2024-07-16 14:27:02 +0300

    add initial liquibase schema, fix price type in Product

--- src/main/java/by/peachack/store/domain/Product.java
+++ src/main/java/by/peachack/store/domain/Product.java
@@ -1,10 +1,12 @@
 package by.peachack.store.domain;
 
+import java.math.BigDecimal;
+
 public class Product {
     private Long id;
     private String name;
     private String description;
     private Category category;
-    private String price;
+    private BigDecimal price;
     private Integer stockQuantity;
 }
