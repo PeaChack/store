3b01ae9fc93b094c708182ff0d9e778f970c3b29
PeaChack <dev.peachack@gmail.com>
2024-07-16 14:46:58 +0300

    config datasource, change domain classes

--- src/main/java/by/peachack/store/domain/Category.java
+++ src/main/java/by/peachack/store/domain/Category.java
@@ -1,7 +1,16 @@
 package by.peachack.store.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
 }
--- src/main/java/by/peachack/store/domain/Product.java
+++ src/main/java/by/peachack/store/domain/Product.java
@@ -1,12 +1,24 @@
 package by.peachack.store.domain;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
+    @Column(name = "price")
     private BigDecimal price;
+    @Column(name = "stock_quantity")
     private Integer stockQuantity;
 }
