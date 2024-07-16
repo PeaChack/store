d62cfd2b4929bbaf3cc98f672540aafb5de4b611
PeaChack <dev.peachack@gmail.com>
2024-07-16 13:46:18 +0300

    add domain classes

---
+++ src/main/java/by/peachack/store/domain/Category.java
@@ -0,0 +1,7 @@
+package by.peachack.store.domain;
+
+public class Category {
+    private Long id;
+    private String name;
+    private String description;
+}
---
+++ src/main/java/by/peachack/store/domain/Product.java
@@ -0,0 +1,10 @@
+package by.peachack.store.domain;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private Category category;
+    private String price;
+    private Integer stockQuantity;
+}
