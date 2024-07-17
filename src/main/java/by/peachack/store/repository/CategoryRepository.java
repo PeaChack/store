da655e3cec22cc56bc23e5bce830417e611d4ab2
PeaChack <dev.peachack@gmail.com>
2024-07-17 22:25:50 +0300

    update CategoryRepository

--- src/main/java/by/peachack/store/repository/CategoryRepository.java
+++ src/main/java/by/peachack/store/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package by.peachack.store.repository;
 import by.peachack.store.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
